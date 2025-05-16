07e8fcba4cefd36e4bcf19046495f4faad1fb7d0
Sarvesh Naik <naiksarvesh463@gmail.com>
2025-05-16 19:20:56 +0530

    remove commented code

--- script.js
+++ script.js
@@ -1,27 +1,6 @@
 const btnConvert = document.querySelector(".btn-convert");
 const resultDiv = document.getElementById("result");
 
-// async function convertCurrency(from, to, amount) {
-//   // Dummy conversion rates
-//   const rates = {
-//     USD: { EUR: 0.91, INR: 83, JPY: 154 },
-//     EUR: { USD: 1.1, INR: 91, JPY: 170 },
-//     INR: { USD: 0.012, EUR: 0.011, JPY: 1.87 },
-//     JPY: { USD: 0.0065, EUR: 0.0058, INR: 0.53 },
-//   };
-
-//   console.log(convert(from, to, amount));
-
-//   if (from === to) return amount;
-//   const rate = rates[from]?.[to];
-
-//   if (!rate) {
-//     throw new Error("Conversion rate is not available");
-//   }
-
-//   return (amount * rate).toFixed(2);
-// }
-
 async function convertCurrency(from, to, amount) {
   const res = await fetch(
     `https://api.frankfurter.app/latest?amount=${amount}&from=${from}&to=${to}`
